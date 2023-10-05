c960dcb773a8cc2f0404af556512c11e2d63f6f8
Nicolas Alejandro Cartalla <ncartalla@uy000d40cg4j796.ml.com>
2023-10-05 12:17:02 -0300

    change script

---
+++ dast/main.py
@@ -0,0 +1,25 @@
+
+from SqlInjection import SqlInjection
+
+BASE_URL = "http://localhost:8080"
+def main():
+    total_exit_codes = 0
+    total_output = ""
+
+    # Login SQL Injection Test
+    total_output += "***** Login SQL Injection Test *****\n"
+    url = BASE_URL+"/doLogin"
+    exit_code, output = SqlInjection.doLoginSqlI(url)
+    total_exit_codes += exit_code
+    total_output += output + "\n"
+    
+    # Otros tests ...
+    
+    # Write report
+    with open('dast-report.txt', 'w') as f:
+        f.write(total_output)    
+    if total_exit_codes == 1:
+        exit(1)
+
+if __name__ == "__main__":
+    main()
