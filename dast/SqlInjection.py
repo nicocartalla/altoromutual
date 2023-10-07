fb90d18832887f5cc36e4b17fba7765985be983b
Nicolas Alejandro Cartalla <nicolas.cartalla@mercadolibre.com>
2023-10-07 18:46:49 -0300

    update pipeline

--- dast/SqlInjection.py
+++ dast/SqlInjection.py
@@ -4,18 +4,20 @@ headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
 }
 
-payloads = [ 
+payloads = [
     "admin",
-    "'"
+    "'",
     "'+--",
     "admin'+or+1=1+--"
 ]
 
 class SqlInjection():
+
     @staticmethod
     def doLoginSqlI(url):
         exit_code = 0
         output = ""
+        
         for payload in payloads:
             data = f"uid={payload}&passw=test&btnSubmit=Login"
             response = requests.post(url, headers=headers, data=data, allow_redirects=False)
@@ -23,17 +25,15 @@ class SqlInjection():
             if response.status_code == 302:
                 if 'Set-Cookie' in response.headers and 'AltoroAccounts' in response.headers['Set-Cookie']:
                     
-                    output += f"[!] uid={payload},Se detectó inyección SQL en el login. \n"
-                    output += f"\t Set-Cookie recibida: {response.headers['Set-Cookie']} \n"
-                    print(f"[!] uid={payload},Se detectó inyección SQL en el login.")
-                    print(f"\t Set-Cookie recibida: {response.headers['Set-Cookie']}")
+                    output += f"[!] uid={payload}, SQL injection detected in the login. \n"
+                    output += f"\t Set-Cookie received: {response.headers['Set-Cookie']} \n"
+                    print(f"[!] uid={payload}, SQL injection detected in the login.")
+                    print(f"\t Set-Cookie received: {response.headers['Set-Cookie']}")
                     exit_code += 1
                 else:
-                    output += f"[*] uid={payload}, Se recibió un {response.status_code}, pero no se encontró la cookie AltoroAccounts. \n"
-                    print(f"[*] uid={payload}, Se recibió un {response.status_code}, pero no se encontró la cookie AltoroAccounts.")
-                    exit_code += 0
+                    output += f"[*] uid={payload}, Received a {response.status_code}, but the AltoroAccounts cookie was not found. \n"
+                    print(f"[*] uid={payload}, Received a {response.status_code}, but the AltoroAccounts cookie was not found.")
             else:
-                output += f"[*] uid={payload}, No se detectó el redireccionamiento {response.status_code} o el login falló. \n"
-                print(f"[*] uid={payload}, No se detectó el redireccionamiento {response.status_code} o el login falló.")
-                exit_code += 0
+                    output += f"[*] uid={payload}, Redirection {response.status_code} not detected or login failed. \n"
+                    print(f"[*] uid={payload}, Redirection {response.status_code} not detected or login failed.")
         return exit_code, output
--- dast/main.py
+++ dast/main.py
@@ -1,24 +1,46 @@
 
 from SqlInjection import SqlInjection
+from Xss import Xss
+import sys
 
-BASE_URL = "http://localhost:8080"
+
+
 def main():
+
+    if len(sys.argv) > 1:
+        BASE_URL = sys.argv[1]
+    else:
+        BASE_URL = "http://localhost:8080"
+
     total_exit_codes = 0
     total_output = ""
+    total_tests = 2
+    tests_passed = 0
 
     # Login SQL Injection Test
-    total_output += "***** Login SQL Injection Test *****\n"
+    total_output += "***** Login - SQL Injection Test *****\n"
     url = BASE_URL+"/doLogin"
     exit_code, output = SqlInjection.doLoginSqlI(url)
     total_exit_codes += exit_code
     total_output += output + "\n"
+    if exit_code == 0:
+        tests_passed += 1
     
     # Otros tests ...
-    
+    total_output += "***** Search - Cross Site Scripting Test  *****\n"
+    url = BASE_URL+"/search.jsp"
+    exit_code, output = Xss.search(url)
+    total_exit_codes += exit_code
+    total_output += output + "\n"
+    if exit_code == 0:
+        tests_passed += 1
+
     # Write report
-    with open('/mnt/dast-report.txt', 'w') as f:
+    total_output += f"Total tests passed: {tests_passed}/{total_tests}\n"
+    print(f"Total tests passed: {tests_passed}/{total_tests}\n")
+    with open('/tmp/dast-report.txt', 'w') as f:
         f.write(total_output)    
-    if total_exit_codes == 1:
+    if total_exit_codes > 0:
         exit(1)
 
 if __name__ == "__main__":
